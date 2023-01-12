62c5608d875e0a850945024d7de3f46f95d18bd3
Manipull <maggelakopoulos@gmail.com>
2023-01-12 19:43:22 +0200

    Update app.js

--- app.js
+++ app.js
@@ -17,82 +17,82 @@ function getPoetry(req, res) {
 }
 
 function getPoetry(req, res) {
-    const id = parseInt(req.params.id)
+    const title = parseInt(req.params.title)
     fs.readFile(aFileName, function (err, data) {
         let poetry = [];
         if (!err) poetry = JSON.parse(data);
-        res.status(200).json(poetry.filter(p => p.id === id));
+        res.status(200).json(poetry.filter(p => p.title === title));
     });
 }
 
 function addPoetry(req, res) {
-    const { id, name, phone, age } = req.body;
-    const newPoetry = {id:parseInt(id), name, phone, age};
+    const { title, date} = req.body;
+    const newPoetry = {title:title, date};
     fs.readFile(aFileName, function (err, data) {
         let poetry = [];
         if (!err) poetry = JSON.parse(data);
         poetry.push(newPoetry);
         fs.writeFile(aFileName,JSON.stringify(poetry),function(err){
                 if (err){
-                    res.status(200).json(`Error adding id: ${id}`);
+                    res.status(200).json(`Error adding title: ${title}`);
                 }
                 else{
-                    res.status(200).json(`Poetry added with id: ${id}`);
+                    res.status(200).json(`Poetry added with title: ${title}`);
                 }
             });
     });
 }
 
 function updatePoetry(req, res) {
-    const { id, name, phone, age } = req.body
-    const aPoetry = {id:parseInt(id), name, phone, age};
+    const { title, date } = req.body
+    const aPoetry = {title:title, date};
     fs.readFile(aFileName, function (err, data) {
         let poetry = [];
         if (!err) poetry = JSON.parse(data);
-        const anIndex = poetry.findIndex(p=>p.id===aPoetry.id);
+        const anIndex = poetry.findIndex(p=>p.title===aPoetry.title);
         if (anIndex < 0 ) {
-            res.status(200).json(`Cannot find ID: ${id}`);
+            res.status(200).json(`Cannot find title: ${title}`);
             return;
         }
         poetry[anIndex] = aPoetry;
         fs.writeFile(aFileName,JSON.stringify(poetry),function(err){
                 if (err){
-                    res.status(200).json(`Error updating id: ${id}`);
+                    res.status(200).json(`Error updating title: ${title}`);
                 }
                 else{
-                    res.status(200).json(`Updated id: ${id}`);
+                    res.status(200).json(`Updated title: ${title}`);
                 }
             });
     });
 }
 
 function deletePoetry(req, res) {
-    const id = parseInt(req.body.id)
+    const title = parseInt(req.body.title)
     fs.readFile(aFileName, function (err, data) {
         let poetry = [];
         if (!err) poetry = JSON.parse(data);
-        const anIndex = poetry.findIndex(p=>p.id===id);
+        const anIndex = poetry.findIndex(p=>p.title===title);
         if (anIndex < 0 ) {
-            res.status(200).json(`Cannot find ID: ${id}`);
+            res.status(200).json(`Cannot find title: ${title}`);
             return;
         }
         poetry.splice(anIndex, 1)
         fs.writeFile(aFileName,JSON.stringify(poetry),function(err){
                 if (err){
-                    res.status(200).json(`Error deleting id: ${id}`);
+                    res.status(200).json(`Error deleting title: ${title}`);
                 }
                 else{
-                    res.status(200).json(`Deleted id: ${id}`);
+                    res.status(200).json(`Deleted title: ${title}`);
                 }
             });
     });
 }
 
 app.get('/api/poetry', (req, res) => getPoetry(req, res));
-app.get('/api/poetry/:id', (req, res) => getPoetry(req, res));
+app.get('/api/poetry/:title', (req, res) => getPoetry(req, res));
 app.post('/api/poetry', (req, res) => addPoetry(req, res));
-app.put('/api/poetry/:id', (req, res) => updatePoetry(req, res));
-app.delete('/api/poetry/:id', (req, res) => deletePoetry(req, res));
+app.put('/api/poetry/:title', (req, res) => updatePoetry(req, res));
+app.delete('/api/poetry/:title', (req, res) => deletePoetry(req, res));
 
 app.use(express.static(__dirname + '/www'));
 const users = {'user1': 'password1','user2': 'password2'};
